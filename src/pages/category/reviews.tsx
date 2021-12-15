ebdfe8035ccd30ad042956a8553117098676034f
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-12-15 12:30:31 -0300

    Refatoração na página Category

---
+++ src/pages/category/reviews.tsx
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+import {
+  defaultLoadPostsVariables,
+  loadPosts,
+  StrapiPostAndSettings
+} from 'api/loadPosts'
+import PostsTemplate from 'templates/PostsTemplate'
+import BaseTemplate from 'templates/Base'
+
+export default function ReviewsPage({
+  posts,
+  setting,
+  variables
+}: StrapiPostAndSettings) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+
+  const { displayName, slug } = posts[0].categories[0]
+
+  return (
+    <>
+      <Head>
+        <title>
+          {displayName} - {setting.blogName}
+        </title>
+      </Head>
+
+      <BaseTemplate settings={setting}>
+        <PostsTemplate
+          posts={posts}
+          variables={variables}
+          category={[displayName, slug]}
+        />
+      </BaseTemplate>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<
+  StrapiPostAndSettings
+> = async () => {
+  let data: StrapiPostAndSettings | null = null
+  const variables = { categorySlug: 'reviews', limit: 20 }
+
+  data = await loadPosts(variables)
+
+  if (!data?.posts.length) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+      variables: {
+        ...defaultLoadPostsVariables,
+        ...variables
+      }
+    },
+    revalidate: 24 * 60 * 60
+  }
+}
