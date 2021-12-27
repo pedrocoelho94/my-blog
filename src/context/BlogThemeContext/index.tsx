6efe2483d707da22e346e2d0c91c17930329cf88
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-12-27 19:20:23 -0300

    Ajustes

--- src/context/BlogThemeContext/index.tsx
+++ src/context/BlogThemeContext/index.tsx
@@ -39,13 +39,32 @@ export const BlogThemeProvider = ({ children }: BlogThemeProviderProps) => {
             primary: '#e7e7e7',
             secondary: '#dc143c',
             darker: '#4d4d4d',
-            darkText: '#e4e4e4',
-            white: '#272727',
-            mediumGray: '#f9f9f9',
-            darkerGray: '#cccccc',
-            lightGray: '#242424'
+            darkText: '#333333',
+            white: '#FAFAFA',
+            mediumGray: '#DDDDDD',
+            darkerGray: '#AAAAAA',
+            lightGray: '#F7F7F7',
+            warning: 'rgb(247, 38, 115)',
+            success: 'rgb(172, 243, 157)',
+            info: 'rgb(85, 193, 255)',
+            deepWhite: 'rgb(255, 255, 255)',
+            gray0: 'rgb(245, 245, 253)',
+            gray1: 'rgb(241, 241, 243)',
+            gray2: 'rgb(221, 221, 223)',
+            gray3: 'rgb(201, 201, 203)',
+            gray4: 'rgb(181, 181, 184)',
+            gray5: 'rgb(161, 161, 164)',
+            gray6: 'rgb(141, 141, 144)',
+            gray7: 'rgb(121, 121, 124)',
+            gray8: 'rgb(101, 101, 104)',
+            gray9: 'rgb(81, 81, 84)',
+            gray10: 'rgb(61, 61, 64)',
+            gray11: 'rgb(41, 41, 44)',
+            gray12: 'rgb(21, 21, 24)',
+            deepBlack: 'rgb(0, 0, 0)'
           }
         }
+
         setBlogTheme(newTheme)
         localStorage.setItem('theme', JSON.stringify(newTheme))
       }
